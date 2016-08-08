9d89311cce3a97273f8b921aa5a1aadc13925bd9
Kelvin Tam <kktam@umich.edu>
2016-08-08 07:22:59 -0500

    new stuffs

--- js/grayscale.js
+++ js/grayscale.js
@@ -190,8 +190,8 @@ $(document).ready(function(){
 $(document).ready(function(){
   $("#e1").click(function() {
     $(".event-info").fadeOut(function() {
-        $(this).html("<p> Lorem dim sum Pot sticker water chestnut cake bitter melon beef dumplings turnip cake leek dumplings deep fried taro turnover " +
-            "Cha siu sou Cheong fan pan fried bitter melon beef dumpling mango pudding.</p>"
+        $(this).html("Lorem dim sum Pot sticker water chestnut cake bitter melon beef dumplings turnip cake leek dumplings deep fried taro turnover " +
+            "Cha siu sou Cheong fan pan fried bitter melon beef dumpling mango pudding."
         ).fadeIn();
     }).fadeIn();
   });
