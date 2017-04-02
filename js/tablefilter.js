295f92edf58045d789e710aae0381b5569177d96
Kelvin Tam <kelvin.tam@live.com>
2017-04-01 23:43:26 -0400

    ker

--- js/tablefilter.js
+++ js/tablefilter.js
@@ -1,3 +1,42 @@
+var cookieList = function(cookieName) {
+  //When the cookie is saved the items will be a comma seperated string
+  //So we will split the cookie by comma to get the original array
+  var cookie = Cookies.get(cookieName);
+
+  //Load the items or a new array if null.
+  var items = cookie ? cookie.split(/,/) : new Array();
+  console.log(items);
+
+  //Return a object that we can use to access the array.
+  //while hiding direct access to the declared items array
+  //this is called closures see http://www.jibbering.com/faq/faq_notes/closures.html
+  return {
+    "add": function(val) {
+        //Add to the items.
+        items.push(val);
+        //Save the items to a cookie.
+        //EDIT: Modified from linked answer by Nick see 
+        //      http://stackoverflow.com/questions/3387251/how-to-store-array-in-jquery-cookie
+        Cookies.set(cookieName, items.join(','));
+    },
+    "remove": function (val) { 
+        //EDIT: Thx to Assef and luke for remove.
+        indx = items.indexOf(val); 
+        if(indx!=-1) items.splice(indx, 1); 
+        Cookies.set(cookieName, items.join(','));        
+    },
+    "clear": function() {
+        items = null;
+        //clear the cookie.
+        Cookies.set(cookieName, null);
+    },
+    "items": function() {
+        //Get all the items.
+        return items;
+    }
+  }
+}
+
 // init Isotope
 var $grid = $('.grid').isotope({
     itemSelector: '.grid-item',
@@ -43,22 +82,30 @@ function concatValues( obj ) {
 
 // for mobile version
 $('.add_wkshp').on('click', function(e) {
-  // console.log("hello");
+  Cookies.set("workshop", "kej");
+  var list = new cookieList("workshops");
   var workshop = $(this).parents('.wk').clone();
   var button = $(workshop).find("i");
-  button.removeClass('add_wkshp');
-  button.removeClass('fa-plus-circle');
-  button.addClass('fa-minus-circle'); 
-  button.addClass('rm_wkshp');
   workshop.removeClass('grid');
   workshop.removeClass('grid-item');
   workshop.css("position", "");
   workshop.css("top", "");
   workshop.css("left", "");
-  var lol = workshop[0].children[1].children;
+  button.removeClass('add_wkshp');
+  button.removeClass('fa-plus-circle');
+  button.addClass('fa-minus-circle'); 
+  button.addClass('rm_wkshp');
   $('#added-workshops').append(workshop);
+  list.add(workshop.attr('id'));
+  console.log(Cookies.get("workshops"));
+  console.log(Cookies.get("workshop"));
 });
 
 $(document).on('click', '.rm_wkshp', function() {
-  $(this).parents('.wk').remove();
+  var workshop = $(this).parents('.wk')
+  var list = new cookieList("workshops");
+  list.remove(workshop.attr('id'));
+  workshop.remove();
+  console.log(list.items());
+
 });
