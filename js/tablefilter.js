f44aeae91466cddc843c32256642f4b66e5cfd89
Kelvin Tam <kelvin.tam@live.com>
2017-04-02 01:39:18 -0400

    elrkjer

--- js/tablefilter.js
+++ js/tablefilter.js
@@ -106,7 +106,7 @@ function loadWorkshops() {
     var workshopId = '#'+workshops[i];
     var workshop = $(workshopId).clone();
     var button = $(workshop).find("i");
-    $('#workshop-list').find(workshopId).find('i').removeClass('fa-plus-circle');
+    $('#workshop-list').find(workshops[i]).find('i').removeClass('fa-plus-circle');
     workshop.removeClass('grid');
     workshop.removeClass('grid-item');
     workshop.css("position", "");
